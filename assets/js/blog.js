3e9f596a6027bc7eb9ea59b5e7bb4238029b4ebd
Zonia Ramirez <jslnramirez@gmail.com>
2024-05-19 23:36:56 -0400

    updated blog and index html, updated form and blog css, and updated blog js

---
+++ assets/js/blog.js
@@ -0,0 +1,16 @@
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
+
+let mode = 'dark';
+
+themeSwitcher.addEventListener('click', function () {
+  if (mode === 'dark') {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+  else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+});
+
